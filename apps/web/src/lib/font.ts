b87e0117348df2b5de85946dd09b787da8babf14
ayush-that <ayush1337@hotmail.com>
2025-11-17 23:08:47 +0530

    ui: change font to inter

--- apps/web/src/lib/font.ts
+++ apps/web/src/lib/font.ts
@@ -1,49 +1,16 @@
-import {
-  Geist,
-  Geist_Mono,
-  Instrument_Sans,
-  Inter,
-  Mulish,
-  Noto_Sans_Mono,
-} from "next/font/google";
-
+import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const fontSans = Geist({
+const appFontSans = Inter({
   subsets: ["latin"],
+  weight: "400",
   variable: "--font-sans",
 });
-
-const fontMono = Geist_Mono({
+const appFontMono = Inter({
   subsets: ["latin"],
+  weight: "400",
   variable: "--font-mono",
 });
 
-const fontInstrument = Instrument_Sans({
-  subsets: ["latin"],
-  variable: "--font-instrument",
-});
-
-const fontNotoMono = Noto_Sans_Mono({
-  subsets: ["latin"],
-  variable: "--font-noto-mono",
-});
-
-const fontMullish = Mulish({
-  subsets: ["latin"],
-  variable: "--font-mullish",
-});
-
-const fontInter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-});
-
-export const fontVariables = cn(
-  fontSans.variable,
-  fontMono.variable,
-  fontInstrument.variable,
-  fontNotoMono.variable,
-  fontMullish.variable,
-  fontInter.variable,
-);
+export const appFontClassName = appFontSans.className;
+export const fontVariables = cn(appFontSans.variable, appFontMono.variable);
